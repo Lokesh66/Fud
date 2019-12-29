fe1bab39d29be8e7c1487002a5e85f1548e2ceeb
Suggu444 <sugunakarnayaka@gmail.com>
2019-12-29 11:04:10 +0530

    - Project Changes

---
+++ FUD/Assets/Scripts/APIConstants.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class APIConstants : MonoBehaviour
+{
+    #region Singleton
+    private static APIConstants instance = null;
+    private APIConstants()
+    {
+
+    }
+
+    public static APIConstants Instance
+    {
+        get
+        {
+            if (instance = null)
+            {
+                instance = FindObjectOfType<APIConstants>();
+            }
+            return instance;
+        }
+    }
+    #endregion
+    public const string SECRET_KEY = "SSht6KyxvwKZnsmbbbQUCm2va";
+    public const string BASE_URL = "http://3.136.106.58:7000/";
+
+    public const string USER = BASE_URL + "v1/users/";
+    public const string USER_LOGIN = BASE_URL + "v1/login/";
+    public const string STORY = BASE_URL + "v1/story/";
+
+    public const string GET_CRATFS = BASE_URL + "v1/24_craftRoles/";
+    public const string GET_GENRES = BASE_URL + "v1/genres/";
+
+
+    public class BaseData
+    {
+        public string message;
+        public int status;
+    }
+
+    #region UserData
+    public class UserData
+    {
+        public int id;
+        public string name;
+        public long phone;
+        public int role_id;
+        public int login_code;
+        public DateTime updatedAt;
+        public DateTime created_date_time;
+    }
+
+    public class UserDataObject : BaseData
+    {
+        public UserData data;
+    }
+    #endregion
+
+    #region UserLogin
+    public class UserLoginData
+    {
+        public int id;
+        public string name;
+        public string token;
+        public long phone;
+        public int role_id;
+        public object plain_id;
+        public int login_code;
+        public int token_expiry;
+        public DateTime created_date_time;
+        public DateTime updatedAt;
+    }
+
+    public class UserLoginbject : BaseData
+    {
+        public UserLoginData data;
+    }
+
+    #endregion
+
+    #region CRAFTS
+    public class Craft
+    {
+        public int id;
+        public string name;
+        public int status;
+        public object image_url;
+        public DateTime created_date_time;
+        public DateTime updatedAt;
+    }
+
+    public class CraftsResponse : BaseData
+    {
+        public List<Craft> data;
+    }
+    #endregion
+
+    #region GENRES
+    public class Genre
+    {
+        public int id;
+        public string name;
+        public DateTime created_date_time;
+        public DateTime? update_date_time;
+        public int? status;
+    }
+
+    public class RootObject : BaseData
+    {
+        public List<Genre> message;
+    }
+    #endregion
+
+}
---
+++ FUD/Assets/Scripts/DownLoadManager.cs
@@ -0,0 +1,90 @@
+﻿/*using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class DownLoadManager : MonoBehaviour
+{
+    #region Imagedownload
+
+    public void DownloadImage(string imageurl, Action<Sprite> CallBack)
+    {
+        if (imageurl.IsNullOrEmpty())
+        {
+            CallBack(null);
+            return;
+        }
+        var m = Regex.Match(imageurl, ".+(/.+)$");
+        string imageName = m.Groups[1].Value.Remove(0, 1);
+        string path = Application.persistentDataPath + "/" + imageName;
+        //path = "file://" + path;
+        if (File.Exists(path))
+        {
+            //path = "file://" + path;
+            //StartCoroutine(GetImage(path, CallBack));
+            LoadPNG(path, CallBack);
+        }
+        else
+        {
+            StartCoroutine(DownloadAndSave(imageurl, CallBack));
+        }
+    }
+
+    IEnumerator DownloadAndSave(string imageurl, Action<Sprite> CallBack)
+    {
+        if (imageurl.IsNOTNullOrEmpty())
+        {
+            WWW www = new WWW(imageurl);
+            yield return www;
+            string response = www.text;
+            if (response.IsNOTNullOrEmpty() && MiniJSON.Json.Deserialize(response) != null)
+            {
+                Dictionary<string, object> jsResponse = MiniJSON.Json.Deserialize(response) as Dictionary<string, object>;
+                object error = jsResponse["error"];
+                if (error != null)
+                {
+                    CallBack(null);
+                }
+            }
+            else if (www.texture != null)
+            {
+                var m = Regex.Match(imageurl, ".+(/.+)$");
+                string imageName = m.Groups[1].Value.Remove(0, 1);
+
+                string path = Application.persistentDataPath + "/" + imageName;
+                Sprite profileSprite = Sprite.Create(www.texture, new Rect(0f, 0f, www.texture.width, www.texture.height), new Vector2(0.5f, 0.5f));//creates sprite from the texture of the image
+                CallBack(profileSprite);
+                byte[] texByte = www.texture.EncodeToPNG();//to convert texture to png
+                File.WriteAllBytes(path, texByte);
+            }
+            else
+            {
+                CallBack(null);
+            }
+            www.Dispose();
+            www = null;
+        }
+        else
+        {
+            CallBack(null);
+        }
+    }
+
+    void LoadPNG(string filePath, Action<Sprite> CallBack)
+    {
+        Texture2D tex = null;
+        byte[] fileData;
+
+        if (File.Exists(filePath))
+        {
+            fileData = File.ReadAllBytes(filePath);
+            tex = new Texture2D(1024, 1024, TextureFormat.DXT1, false);
+            tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+        }
+        Sprite ProfilePic = Sprite.Create(tex, new Rect(0f, 0f, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+        CallBack(ProfilePic);
+    }
+}
+*/
