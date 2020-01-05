93f507f70afaa61418a26c21741c5dc636073e68
Shankar <dorababukarri@gmail.com>
2020-01-05 20:34:43 +0530

    Login/SignIn Scene updated

---
+++ FUD/Assets/Scripts/Login/DetailsScreen.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class DetailsScreen : MonoBehaviour
+{
+    public TMP_InputField mobileNumberTextField;
+
+    System.Action<bool, string> OnButtonAction;
+
+    public void SetView(System.Action<bool, string> action)
+    {
+        gameObject.SetActive(true);
+
+        mobileNumberTextField.text = "";
+        OnButtonAction = action;
+    }
+
+    public void OnClick_SendOTP()
+    {
+        GameManager.Instance.apiHandler.SendOTP(mobileNumberTextField.text, (bool status) => {
+            if (status)
+            {
+                OnButtonAction?.Invoke(true, mobileNumberTextField.text);
+            }
+            else
+            {
+                //Erros message
+            }
+        });
+    }
+
+    public void OnClick_Back()
+    {
+        OnButtonAction?.Invoke(false, null);
+    }
+}
---
+++ FUD/Assets/Scripts/Login/LaunchScreen.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public enum ELoginButtonType
+{
+    None,
+    Login,
+    SignUp
+}
+public class LaunchScreen : MonoBehaviour
+{
+    System.Action<ELoginButtonType> OnButtonPressed;
+
+    public void SetView(System.Action<ELoginButtonType> action)
+    {
+        gameObject.SetActive(true);
+        OnButtonPressed = action;
+    }
+
+    public void OnClick_SignIn()
+    {
+        OnButtonPressed?.Invoke(ELoginButtonType.SignUp);
+        OnButtonPressed = null;
+    }
+
+    public void OnClick_Login()
+    {
+        OnButtonPressed?.Invoke(ELoginButtonType.Login);
+        OnButtonPressed = null;
+    }
+
+}
