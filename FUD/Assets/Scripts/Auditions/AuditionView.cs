887c1c4f1d42a11184bba7801779720603e48c76
Suggu444 <sugunakarnayaka@gmail.com>
2020-02-02 10:05:35 +0530

    - Added Base scripts for audition, projects and portfolio

---
+++ FUD/Assets/Scripts/Auditions/AuditionView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AuditionView : BaseView
+{
+    protected override void EnableView()
+    {
+        base.EnableView();
+    }
+
+    protected override void OnAddSubView(GameObject addedObject)
+    {
+        base.OnAddSubView(addedObject);
+    }
+
+    public override void OnRemoveLastSubView()
+    {
+        base.OnRemoveLastSubView();
+    }
+
+    public override void OnExitScreen()
+    {
+        base.OnExitScreen();
+    }
+}
