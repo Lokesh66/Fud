b715295666c20827b7be14a97f30d820eddc9949
Suggu444 <sugunakarnayaka@gmail.com>
2020-02-08 17:38:44 +0530

    - missing scripts added

---
+++ FUD/Assets/Scripts/Common/NoDataView.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System;
+using TMPro;
+
+
+public class NoDataView : MonoBehaviour
+{
+    public TextMeshProUGUI subTitleText;
+
+    public TextMeshProUGUI buttonText;
+
+    public Button createButton;
+
+
+    public void SetView(NoDataModel noDataModel)
+    {
+        subTitleText.text = noDataModel.subTitle;
+
+        buttonText.text = noDataModel.buttonName;
+
+        createButton.onClick.RemoveAllListeners();
+
+        createButton.onClick.AddListener(delegate { noDataModel.buttonAction?.Invoke(); });
+    }
+}
+
+[Serializable]
+public class NoDataModel
+{
+    public string subTitle;
+    public string buttonName;
+    public Action buttonAction;
+}
