6d517f610708bf41a865263520b9116bebba3ba6
Suggu444 <sugunakarnayaka@gmail.com>
2020-02-13 02:03:57 +0530

    - Removed unused code

--- FUD/Assets/Scripts/MenuScreen/HomeView.cs
+++ FUD/Assets/Scripts/MenuScreen/HomeView.cs
@@ -4,16 +4,6 @@ public class HomeView : BaseView
 {
     public GameObject leftMenuPanel;
 
-    public GameObject projectCreationPanel;
-
-    public GameObject projectsPanel;
-
-    public StoryCreationView storyCreationView;
-
-    public ProjectController projectController;
-
-    public MyStoriesView myStoriesView;
-
 
     #region Ovveride Methods
     protected override void EnableView()
@@ -41,34 +31,6 @@ public class HomeView : BaseView
     public void OnMenuButtonAction()
     {
         leftMenuPanel.SetActive(true);
-    }
-
-    public void OnProjectCreateAction()
-    {
-        projectCreationPanel.SetActive(true);
-
-        leftMenuPanel.SetActive(false);
-    }
-
-    public void OnProjectsAction()
-    {
-        projectController.Load();
-
-        leftMenuPanel.SetActive(false);
-    }
-    public void OnCreateStoryAction()
-    {
-        Debug.Log("Persistent Path = " + APIConstants.PERSISTENT_PATH);
-        leftMenuPanel.SetActive(false);
-
-        //storyCreationView.Load();
-    }
-
-    public void OnMyStoriesAction()
-    {
-        leftMenuPanel.SetActive(false);
-
-        myStoriesView.Load();
     }
 
     public void OnCloseLeftMenu()
