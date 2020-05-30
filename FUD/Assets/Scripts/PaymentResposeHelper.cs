ec033f23ae259a45032f0bf0bd031ce4c2ecd296
ravirajvaidya <ravirajvaidya06@gmail.com>
2020-05-30 11:48:03 +0530

    Plugin Integration WIP

---
+++ FUD/Assets/Scripts/PaymentResposeHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaymentResposeHelper : MonoBehaviour
+{
+    AndroidJavaClass ajc;
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        ajc = new AndroidJavaClass("phonatoto.cashfree.PaymentActivity");
+        //SendToAndroid("Hello World");
+    }
+    private void SendToAndroid(string message)
+    {
+        ajc.CallStatic("ShowToast", message);
+    }
+    // This function will be called from Android
+    private void onPaymentResponseRecieved(string txStatus, string referenceId)
+    {
+        Debug.Log("Received message from plugin : txStatus : " + txStatus + " referenceId : " + referenceId);
+    }
+}
