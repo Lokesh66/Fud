87ed40b3b6d8cd4f5685f7f43b63e861f7cc5eb9
Sugunakar <sugunakar.nayaka@yesgnome.com>
2020-07-31 23:12:35 +0530

    - Media Source updated

--- FUD/Assets/Scripts/API/APIHandler.cs
+++ FUD/Assets/Scripts/API/APIHandler.cs
@@ -83,224 +83,12 @@ public partial class APIHandler
     }
     #endregion
 
-    #region Imagedownload
-
-    public void DownloadImage(string imageurl, Action<Sprite> CallBack)
-    {
-        if (string.IsNullOrEmpty(imageurl))//.IsNullOrEmpty())
-        {
-            CallBack(null);
-            return;
-        }
-        var m = Regex.Match(imageurl, ".+(/.+)$");
-        string imageName = m.Groups[1].Value.Remove(0, 1);
-        string path = Application.persistentDataPath + "/" + imageName;
-        //path = "file://" + path;
-        if (File.Exists(path))
-        {
-            //path = "file://" + path;
-            //StartCoroutine(GetImage(path, CallBack));
-            LoadPNG(path, CallBack);
-        }
-        else
-        {
-            gameManager.StartCoroutine(DownloadAndSave(imageurl, CallBack));
-        }
-    }
-
-    IEnumerator DownloadAndSave(string imageurl, Action<Sprite> CallBack)
-    {
-        if (!string.IsNullOrEmpty(imageurl))//.IsNOTNullOrEmpty())
-        {
-            WWW www = new WWW(imageurl);
-            yield return www;
-            string response = www.text;
-            if (!string.IsNullOrEmpty(response) && MiniJSON.Json.Deserialize(response) != null)
-            {
-                Dictionary<string, object> jsResponse = MiniJSON.Json.Deserialize(response) as Dictionary<string, object>;
-                object error = jsResponse["error"];
-                if (error != null)
-                {
-                    CallBack(null);
-                }
-            }
-            else if (www.texture != null)
-            {
-                var m = Regex.Match(imageurl, ".+(/.+)$");
-                string imageName = m.Groups[1].Value.Remove(0, 1);
-
-                string path = Application.persistentDataPath + "/" + imageName;
-                Sprite profileSprite = Sprite.Create(www.texture, new Rect(0f, 0f, www.texture.width, www.texture.height), new Vector2(0.5f, 0.5f));//creates sprite from the texture of the image
-                CallBack(profileSprite);
-                byte[] texByte = www.texture.EncodeToPNG();//to convert texture to png 
-                File.WriteAllBytes(path, texByte);
-            }
-            else
-            {
-                CallBack(null);
-            }
-            www.Dispose();
-            www = null;
-        }
-        else
-        {
-            CallBack(null);
-        }
-    }
-
-    void LoadPNG(string filePath, Action<Sprite> CallBack)
-    {
-        Texture2D tex = null;
-        byte[] fileData;
-
-        if (File.Exists(filePath))
-        {
-            fileData = File.ReadAllBytes(filePath);
-            tex = new Texture2D(1024, 1024, TextureFormat.DXT1, false);
-            tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
-        }
-        Sprite ProfilePic = Sprite.Create(tex, new Rect(0f, 0f, tex.width, tex.height), new Vector2(0.5f, 0.5f));
-        CallBack(ProfilePic);
-    }
-
-    IEnumerator GetImage(string path, Action<Sprite> CallBack)
-    {
-        WWW www = new WWW(path);
-        yield return www;
-        Texture2D texTmp = new Texture2D(1024, 1024, TextureFormat.DXT1, false);
-        //LoadImageIntoTexture compresses JPGs by DXT1 and PNGs by DXT5     
-        www.LoadImageIntoTexture(texTmp);
-
-        Sprite ProfilePic = Sprite.Create(www.texture, new Rect(0f, 0f, www.texture.width, www.texture.height), new Vector2(0.5f, 0.5f));
-        CallBack(ProfilePic);
-    }
-
     public enum ImageFilterMode : int
     {
         Nearest = 0,
         Biliner = 1,
         Average = 2
     }
-
-    public static Texture2D ResizeTexture(Texture2D pSource, ImageFilterMode pFilterMode, float pScale)
-    {
-
-        //			byte[] response = null;
-
-
-        //*** Variables
-        int i;
-
-        //*** Get All the source pixels
-        Color[] aSourceColor = pSource.GetPixels(0);
-        Vector2 vSourceSize = new Vector2(pSource.width, pSource.height);
-
-        //*** Calculate New Size
-        float xWidth = pSource.width / 2;//Mathf.RoundToInt ((float)pSource.width / 2 * pScale);                     
-        float xHeight = pSource.height / 2;//Mathf.RoundToInt ((float)pSource.height / 2 * pScale);
-
-        //*** Make New
-        Texture2D oNewTex = new Texture2D((int)xWidth, (int)xHeight, TextureFormat.RGBA32, false);
-
-        //*** Make destination array
-        int xLength = (int)xWidth * (int)xHeight;
-        Color[] aColor = new Color[xLength];
-
-        Vector2 vPixelSize = new Vector2(vSourceSize.x / xWidth, vSourceSize.y / xHeight);
-
-        //*** Loop through destination pixels and process
-        Vector2 vCenter = new Vector2();
-        for (i = 0; i < xLength; i++)
-        {
-
-            //*** Figure out x&y
-            float xX = (float)i % xWidth;
-            float xY = Mathf.Floor((float)i / xWidth);
-
-            //*** Calculate Center
-            vCenter.x = (xX / xWidth) * vSourceSize.x;
-            vCenter.y = (xY / xHeight) * vSourceSize.y;
-
-            //*** Do Based on mode
-            //*** Nearest neighbour (testing)
-            if (pFilterMode == ImageFilterMode.Nearest)
-            {
-
-                //*** Nearest neighbour (testing)
-                vCenter.x = Mathf.Round(vCenter.x);
-                vCenter.y = Mathf.Round(vCenter.y);
-
-                //*** Calculate source index
-                int xSourceIndex = (int)((vCenter.y * vSourceSize.x) + vCenter.x);
-
-                //*** Copy Pixel
-                aColor[i] = aSourceColor[xSourceIndex];
-            }
-
-            //*** Bilinear
-            else if (pFilterMode == ImageFilterMode.Biliner)
-            {
-
-                //*** Get Ratios
-                float xRatioX = vCenter.x - Mathf.Floor(vCenter.x);
-                float xRatioY = vCenter.y - Mathf.Floor(vCenter.y);
-
-                //*** Get Pixel index's
-                int xIndexTL = (int)((Mathf.Floor(vCenter.y) * vSourceSize.x) + Mathf.Floor(vCenter.x));
-                int xIndexTR = (int)((Mathf.Floor(vCenter.y) * vSourceSize.x) + Mathf.Ceil(vCenter.x));
-                int xIndexBL = (int)((Mathf.Ceil(vCenter.y) * vSourceSize.x) + Mathf.Floor(vCenter.x));
-                int xIndexBR = (int)((Mathf.Ceil(vCenter.y) * vSourceSize.x) + Mathf.Ceil(vCenter.x));
-
-                //*** Calculate Color
-                aColor[i] = Color.Lerp(
-                    Color.Lerp(aSourceColor[xIndexTL], aSourceColor[xIndexTR], xRatioX),
-                    Color.Lerp(aSourceColor[xIndexBL], aSourceColor[xIndexBR], xRatioX),
-                    xRatioY
-                );
-            }
-
-            //*** Average
-            else if (pFilterMode == ImageFilterMode.Average)
-            {
-
-                //*** Calculate grid around point
-                int xXFrom = (int)Mathf.Max(Mathf.Floor(vCenter.x - (vPixelSize.x * 0.5f)), 0);
-                int xXTo = (int)Mathf.Min(Mathf.Ceil(vCenter.x + (vPixelSize.x * 0.5f)), vSourceSize.x);
-                int xYFrom = (int)Mathf.Max(Mathf.Floor(vCenter.y - (vPixelSize.y * 0.5f)), 0);
-                int xYTo = (int)Mathf.Min(Mathf.Ceil(vCenter.y + (vPixelSize.y * 0.5f)), vSourceSize.y);
-
-                //*** Loop and accumulate
-                //					Vector4 oColorTotal = new Vector4 ();
-                Color oColorTemp = new Color();
-                float xGridCount = 0;
-                for (int iy = xYFrom; iy < xYTo; iy++)
-                {
-                    for (int ix = xXFrom; ix < xXTo; ix++)
-                    {
-
-                        //*** Get Color
-                        oColorTemp += aSourceColor[(int)(((float)iy * vSourceSize.x) + ix)];
-
-                        //*** Sum
-                        xGridCount++;
-                    }
-                }
-
-                //*** Average Color
-                aColor[i] = oColorTemp / (float)xGridCount;
-            }
-        }
-
-        //*** Set Pixels
-        oNewTex.SetPixels(aColor);
-        oNewTex.Apply();
-        return oNewTex;
-
-
-        //*** Return
-    }
-
-    #endregion
 
     #region Get Headers
 
@@ -535,7 +323,7 @@ public partial class APIHandler
         }
     }
 
-    IEnumerator Upload(string filePath, EMediaType mediaType, Action<bool, string> responseCallBack)
+    IEnumerator Upload(string filePath, EMediaType mediaType, string mediaSource, Action<bool, string> responseCallBack)
     {
         byte[] img = File.ReadAllBytes(filePath);
 
@@ -554,7 +342,7 @@ public partial class APIHandler
             form.AddBinaryData("file", img, filePath, "video/*");
         }
 
-        form.AddField("fromsignup", "true");
+        form.AddField("image_source", mediaSource);
 
         UnityWebRequest request = UnityWebRequest.Post(APIConstants.MEDIA_URL, form);
 
---
+++ FUD/Build/CFViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CFViewController.swift
+//  Unity-iPhone
+//
+//  Created by Sugunakar on 7/25/20.
+//
+
+import UIKit
+
+class CFViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
